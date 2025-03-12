6fe3ad0b31f16dd8cd35b77b0937037cb87cc2bf
kauan moreira <kauan.moreira@prodepa.pa.gov.br>
2025-03-12 15:58:29 -0300

    add

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/ka/Área de Trabalho/python-django/venv'
+set -gx VIRTUAL_ENV /home/perry/Desktop/python-django/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
